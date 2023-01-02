56f0e07883d030f45cb6a8d1dfa7820e6d6b8883
Eric Phung <esphung@gmail.com>
2023-01-01 22:35:06 -0600

    added testfunc

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,12 +3,19 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Auth0 from 'react-native-authzero';
 console.log({Auth0})
-const {logout, multiply, useAuth0}= Auth0
+const {logout, test, multiply, useAuth0}= Auth0
 
 multiply(3,4).then((product)=> {
   console.log(product)
 })
 
+
+test("Hello").then((result)=> {
+  console.log({result})
+})
+
+let a = logout()//.then(logoutResp => { console.log(logoutResp)})
+console.log({a})
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
   const useAuth0Methods = useAuth0()
@@ -18,7 +25,6 @@ export default function App() {
   React.useEffect(() => {
     multiply(3, 7).then(setResult);
     console.log({logout})
-   // logout().then(logoutResp => { console.log({logoutResp})})
   }, []);
 
   return (
